30781087941e6b39dd7d9831cb70730dec3d63e2
ARamos <albertramos902@gmail.com>
2024-04-28 15:59:17 +0100

    svg causing error on production, changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,6 +13,7 @@ const FooterContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  gap: 1rem;
   background-color: black;
   border-top: 1px rgba(255, 255, 255, 1) solid;
 `;
@@ -39,7 +40,7 @@ const Footer = () => {
   return (
     <>
       <FooterContainer>
-         <svg
+        <svg
       xmlns="http://www.w3.org/2000/svg"
       width="22"
       height="22"
@@ -55,9 +56,8 @@ const Footer = () => {
       <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
     </svg>
         <a href={githubUrl} target="_blank" rel="noopener noreferrer">
-          
+        <p style={{ fontSize: "0.8rem", paddingLeft: '8px' }}> © BertoRamosM</p>  
         </a>
-        <p style={{ fontSize: "0.8rem" }}> © BertoRamosM</p>
 
         <ModeIcon
           src={currentMode === "light" ? sun : moon}
